705f20a1f058416f712cc04135a49e239a6a242f
Emeka Orji <emekapraiseo@gmail.com>
2021-07-01 21:22:15 +0100

    Modified js file
    Added functionality for the newly added Perimeter Calculation Layout

--- Triangles/js/triangle.js
+++ Triangles/js/triangle.js
@@ -1,11 +1,67 @@
-var bb = document.getElementById("base");
-var hh = document.getElementById("height");
+var bh = document.querySelectorAll("#area-container input");
+var side = document.querySelectorAll("#perimeter-container input");
+var aHead = document.querySelector("#area-header");
+var pHead = document.querySelector("#perimeter-header");
+var aTab = document.querySelector("#area");
+var pTab = document.querySelector("#perimeter");
+var aContainer = document.querySelector("#area-container");
+var pContainer = document.querySelector("#perimeter-container");
+var fixBg = document.querySelector("#fixedbg");
+var menu = document.querySelector("#menu-btn");
+var sidebar = document.querySelector(".sidebar-container");
 
-function solve() {
-  var b = bb.value;
-  var h = hh.value;
-  document.getElementById("answer").textContent = b * h * 0.5;
+aTab.addEventListener("click", function () {
+  pContainer.classList.add("notactive");
+  aContainer.classList.remove("notactive");
+  this.classList.add("active");
+  pTab.classList.remove("active");
+  tuckIn();
+});
+pTab.addEventListener("click", function () {
+  aContainer.classList.add("notactive");
+  pContainer.classList.remove("notactive");
+  this.classList.add("active");
+  aTab.classList.remove("active");
+  tuckIn();
+});
+bh.forEach(function (e, i, arr) {
+  e.addEventListener("input", solveArea);
+  function solveArea() {
+    var header = document.querySelector(".active-head-a").textContent;
+    var b = arr[0].value;
+    var h = arr[1].value;
+    if (header === "Calculate the Area of a Triangle") {
+      document.getElementById("a-answer").textContent = b * h * 0.5;
+    }
+  }
+});
+for (let i = 0; i < side.length; i++) {
+  const e = side[i];
+  e.addEventListener("input", solvePerimeter);
+  function solvePerimeter() {
+    let header = document.querySelector(".active-head-p").textContent;
+    let s1 = Number(side[0].value);
+    let s2 = Number(side[1].value);
+    let s3 = Number(side[2].value);
+    if (header === "Calculate the Perimeter of a Triangle") {
+      document.getElementById("p-answer").textContent = s1 + s2 + s3;
+    }
+  }
 }
-
-bb.addEventListener("input", solve);
-hh.addEventListener("input", solve);
+pContainer.addEventListener("click", tuckIn);
+aContainer.addEventListener("click", tuckIn);
+fixBg.addEventListener("click", tuckIn);
+menu.addEventListener("focus", tuckOut);
+function tuckOut() {
+  sidebar.classList.add("tuckout");
+  sidebar.classList.remove("tuckin");
+}
+function tuckIn() {
+  sidebar.classList.add("tuckin");
+  sidebar.classList.remove("tuckout");
+}
+document.body.addEventListener("keyup", function (e) {
+  if (e.keyCode == 9) {
+    tuckOut();
+  }
+});
